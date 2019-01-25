941f508d50ee22cffea7c7ee3dbf27e8d7e381bc
Sergej <driverrails@gmail.com>
2019-01-25 10:41:43 +0200

    added seeeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  c = FactoryBot.create(:company)
+  FactoryBot.create_list(:claim, 10, company: c)
+  FactoryBot.create(:contact, company: c)
+end
